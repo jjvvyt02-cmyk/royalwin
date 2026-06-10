6d664a247b834787c23be0af1aa5971593e30ffe
jjvvyt02-cmyk <jjvvyt02@gmail.com>
2026-06-11 01:27:07 +0530

    Update bot-v2.js

--- bot-v2.js
+++ bot-v2.js
@@ -1,1 +1,56 @@
+const admin = require('firebase-admin');
+const https = require('https');
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_JSON);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    'https://royalwin-32d97-default-rtdb.asia-southeast1.firebasedatabase.app'
+});
+
+const db = admin.database();
+
+function sendTelegram(text) {
+  return new Promise((resolve, reject) => {
+    const payload = JSON.stringify({
+      chat_id: process.env.TELEGRAM_CHANNEL,
+      text
+    });
+
+    const req = https.request(
+      `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(payload)
+        }
+      },
+      res => {
+        let data = '';
+
+        res.on('data', chunk => {
+          data += chunk;
+        });
+
+        res.on('end', () => {
+          console.log(data);
+          resolve(data);
+        });
+      }
+    );
+
+    req.on('error', reject);
+    req.write(payload);
+    req.end();
+  });
+}
+
+async function runBot() {
+  console.log('BOT V2 STARTED');
+}
+
+runBot();
+
+setInterval(runBot, 30000);
